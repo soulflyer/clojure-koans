2d2673fbddc4aaede873337ee434c2eadbea2214
iain <iain@soulflyer.co.uk>
2017-07-06 20:41:08 +0700

    done

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,29 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [in-coll coll
+         out-coll  '()]
+    (if (= [] in-coll)
+      out-coll
+      (recur (rest in-coll) (conj out-coll (first in-coll))))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= n 0)
+             acc
+             (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
